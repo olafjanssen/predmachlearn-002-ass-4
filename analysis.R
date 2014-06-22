a8317ab88c2560f6d0d8cf2bf95d9e474dab8ed9
Olaf Janssen <o.t.a.janssen@gmail.com>
2014-06-23 00:29:52 +0200

    Add partitioning of the data and first draft of assignment html

--- analysis.R
+++ analysis.R
@@ -7,8 +7,13 @@ library(caret)
 registerDoMC(cores = 7)
 
 # load training and testing data
-training <- read.csv("../pml-training.csv")
-testing <- read.csv("../pml-testing.csv")
+data <- read.csv("../pml-training.csv")
+measurements <- read.csv("../pml-testing.csv")
+
+# partition the data into training and testing set
+inTrain <- createDataPartition(y=data$classe, p=0.7, list=FALSE)
+training <- data[inTrain,]
+testing <- data[-inTrain,]
 
 # extract testable part of the training set
 predictables <- !is.na(testing[1,])
@@ -16,9 +21,10 @@ predictables <- !is.na(testing[1,])
 # clean the data
 training <- training[, predictables]
 testing <- testing[, predictables]
+measurements <- measurements[, predictables]
 
 # train using random forest training
-rfFit <- train(classe ~ . - X - user_name - raw_timestamp_part_1 - raw_timestamp_part_2 - new_window - num_window, data = training, method = "rf", importance = T)
+rfFit <- train(classe ~ . - X - user_name - raw_timestamp_part_1 - raw_timestamp_part_2 - new_window - num_window, data = training, method = "rf", importance = T, PROX=TRUE)
 
 # prediction
-answers <- predict(rfFit, testing)
+answers <- predict(rfFit, measurements)
